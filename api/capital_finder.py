152c0368aabfe0f26bf45a31ddeeb751649f2832
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-06-13 03:59:07 +0000

    test

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+import requests
  
 class handler(BaseHTTPRequestHandler):
  
@@ -14,7 +15,17 @@ class handler(BaseHTTPRequestHandler):
     str_test = "hello"
     
     if country and capital:
-       respons = f"The capital of {country} is {capital}."
+       api_URL = f'https://restcountries.com/v3.1/name/{country}'
+       res = requests.get(api_URL)
+       data = res.json()
+       capital = data[0]["name"][0]
+       if country == str(data[0]["name"]["common"]).lower() and capital == str(data[0]["capital"][0]).lower():
+          respons = "Yes, "
+       else:
+          respons = "No, "
+          country = data[0]["name"]["common"]
+          capital = data[0]["capital"][0]
+       respons += f"The capital of {country} is {capital}."
     elif my_dict.get('country'):
         respons = f"The capital of {country} is {str_test}."
     elif my_dict.get('capital'):
@@ -23,6 +34,6 @@ class handler(BaseHTTPRequestHandler):
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
-    
+
     self.wfile.write(respons.encode())
     return
