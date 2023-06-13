4e78b6db9a257081c0862c64e5e22a63f0f68d18
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-06-13 04:03:17 +0000

    test

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -15,17 +15,17 @@ class handler(BaseHTTPRequestHandler):
     str_test = "hello"
     
     if country and capital:
-       api_URL = f'https://restcountries.com/v3.1/name/{country}'
-       res = requests.get(api_URL)
-       data = res.json()
-       capital = data[0]["name"][0]
-       if country == str(data[0]["name"]["common"]).lower() and capital == str(data[0]["capital"][0]).lower():
-          respons = "Yes, "
-       else:
-          respons = "No, "
-          country = data[0]["name"]["common"]
-          capital = data[0]["capital"][0]
-       respons += f"The capital of {country} is {capital}."
+    #    api_URL = f'https://restcountries.com/v3.1/name/{country}'
+    #    res = requests.get(api_URL)
+    #    data = res.json()
+    #    capital = data[0]["name"][0]
+    #    if country == str(data[0]["name"]["common"]).lower() and capital == str(data[0]["capital"][0]).lower():
+    #       respons = "Yes, "
+    #    else:
+    #       respons = "No, "
+    #       country = data[0]["name"]["common"]
+    #       capital = data[0]["capital"][0]
+       respons = f"The capital of {country} is {capital}."
     elif my_dict.get('country'):
         respons = f"The capital of {country} is {str_test}."
     elif my_dict.get('capital'):
