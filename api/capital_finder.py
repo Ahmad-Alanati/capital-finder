00554e121bb55515f3e0224525747517bf89cece
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-06-13 03:25:27 +0000

    test

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -9,12 +9,14 @@ class handler(BaseHTTPRequestHandler):
     self.end_headers()
     my_URL_path = self.path
     my_URL_components = parse.urlsplit(my_URL_path)
+    print(my_URL_components)
     query_list = parse.parse_qsl(my_URL_components.query)
     my_dict = dict(query_list)
     if my_dict.get('country'):
         country= my_dict.get('country')
     elif my_dict.get('capital'):
         capital =  my_dict.get('capital')
+    
     
     respons = f"The capital of {country} is {capital}."
     self.wfile.write(respons.encode())
