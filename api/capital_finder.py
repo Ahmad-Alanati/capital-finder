30934de5eac81dd1ab37b7ca7c37bc4c7b2832e7
DarkStars0 <ahmadjehadalanati1@gmail.com>
2023-06-13 03:33:54 +0000

    test

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -4,24 +4,20 @@ from urllib import parse
 class handler(BaseHTTPRequestHandler):
  
   def do_GET(self):
-    url_path = self.path
-    # print(1111,url_path)
-    url_components = parse.urlsplit(url_path)
-    # print(2222,url_components)
-    query_list = parse.parse_qsl(url_components.query)
-    # print(33333,query_list)
+    my_URL_path = self.path
+    my_URL_components = parse.urlsplit(my_URL_path)
+    query_list = parse.parse_qsl(my_URL_components.query)
     my_dict = dict(query_list)
-    # print(44444,my_dict)
-    # name = my_dict['name']
-    name = my_dict.get('name')
 
-    if name :
-      message = f'welcome {name}'
-    else: 
-        message = f'welcome stranger'
+    if my_dict.get('country'):
+        country= my_dict.get('country')
+    elif my_dict.get('capital'):
+        capital =  my_dict.get('capital')
+    
     self.send_response(200)
     self.send_header('Content-type', 'text/plain')
     self.end_headers()
 
-    self.wfile.write(message.encode())
+    respons = f"The capital of {country} is {capital}."
+    self.wfile.write(respons.encode())
     return
